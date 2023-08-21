0bf6299abbcb416cf882bff115139eda964dd0ec
kiplangatwayne <waynekiplangatkorir903@gmail.com>
2023-08-21 10:20:40 +0300

    completed Assingment

--- lib/oxford_comma.py
+++ lib/oxford_comma.py
@@ -1,2 +1,10 @@
-def oxford_comma(items):
-    return None
+def oxford_comma(lst):
+    if len(lst) == 1:
+        return lst[0]
+
+    elif len(lst) == 2:
+        return f"{lst[0]} and {lst[1]}"
+
+    else:
+        formatted_list = ', '.join(lst[:-1])  # Join all elements except the last one with commas
+        return f"{formatted_list}, and {lst[-1]}"  # Add the last element with "and"
